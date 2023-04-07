aac869e63a4d08603f61e35f5d77950b3f6ae7b0
Saravanan-1213mm <saravanansekar033@gmail.com>
2023-04-07 22:58:56 +0530

    backend api added

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ import io from "socket.io-client";
 import "./App.css";
 
 const Home = () => {
-  const socket = io("http://localhost:5000");
+  const socket = io("https://zoom-clone-backend.onrender.com/");
 
   const [code, setCode] = useState();
   const [copys, setCopys] = useState(false);
